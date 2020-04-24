6f3acb88856ff57077f1a49c8bd3de958b3ff7a0
tuanhuy180903 <49939079+tuanhuy180903@users.noreply.github.com>
2020-04-24 15:44:25 +0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 # bot.py
 import os
-import random
 import json
 
 import discord
@@ -10,6 +9,21 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 #bot.remove_command('help')
+
+def get_dirty(file):
+    with open(file,'r') as f:
+        dirty = f.readlines()
+    for i in range(len(dirty)):
+        dirty[i] = dirty[i].strip('\n')
+    return dirty
+
+def check_dirty(message):
+    bad_words = get_dirty('dirty.txt')
+    for word in bad_words:
+        mes_low = message.content.lower()
+        if mes_low.count(word.lower()) > 0:
+            return True
+    return False
 
 def get_prefix(bot, message):
     key = str(message.author.id)
@@ -28,31 +42,111 @@ bot = commands.Bot(command_prefix=get_prefix)
 @bot.event
 async def on_ready():
     await bot.change_presence(status=discord.Status.idle, activity=discord.Activity(name="this server", type=discord.ActivityType.watching))
-    channel = discord.utils.get(bot.get_all_channels(), name = 'command')
-    await channel.send('Hey I\'m online')
+    channel = discord.utils.get(bot.get_all_channels(), name = 'chat')
+    await channel.send('Hey I\'m online!')
 
-@bot.command()
-@commands.has_permissions(administrator=True)
+@bot.command(description='Only for the developer!')
+@commands.is_owner()
 async def load(ctx, extension):
     bot.load_extension(f'cogs.{extension}')
 
-@bot.command()
-@commands.has_permissions(administrator=True)
+@load.error
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CheckFailure):
+        return await ctx.send('Sorry, you found a developer-only command!')
+
+@bot.command(description='Only for the developer!')
+@commands.is_owner()
 async def unload(ctx, extension):
     bot.unload_extension(f'cogs.{extension}')
 
-@bot.command(aliases=['rl'])
-@commands.has_permissions(administrator=True)
-async def reload(ctx, extension):
+@unload.error
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CheckFailure):
+        return await ctx.send('Sorry, you found a developer-only command!')
+
+@bot.command(aliases=['rl'],description='Only for the developer!')
+@commands.is_owner()
+async def reload_cogs(ctx, extension):
     bot.unload_extension(f'cogs.{extension}')   
     bot.load_extension(f'cogs.{extension}')
     await ctx.send(f'Reload `{extension}` completed')
 
+@reload_cogs.error
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CheckFailure):
+        return await ctx.send('Sorry, you found a developer-only command!')
+
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         bot.load_extension(f'cogs.{filename[:-3]}')
-    
-@bot.command(name='99', help='Responds with a random quote from Brooklyn 99')
+
+@bot.event
+async def on_message(message):
+    if message.channel.type == discord.ChannelType.private:
+        return
+    if (message.author == bot.user) or (message.author.top_role.name == 'Teacher'):
+        return await bot.process_commands(message)
+    if message.channel.name == 'rules':
+        if message.content.startswith(';signup'):
+            if check_dirty(message):
+                await message.channel.purge(limit=1)
+            else:
+                await bot.process_commands(message)
+                await message.channel.purge(limit=1)
+        else:
+            await message.channel.purge(limit=1)
+    else:
+        if check_dirty(message):
+            await message.channel.purge(limit=1)
+            await message.channel.send('Warning! A bad word was said!')
+        else:
+            await bot.process_commands(message)
+
+@bot.command(name='ping', help='Show ping.')
+async def ping(ctx):
+    await ctx.send(f'Pong! In {round(bot.latency*1000)}ms.')
+  
+@bot.command(aliases=['create','cr'], help='Create a channel',description='Only for the developer!')
+@commands.is_owner()
+async def create_channel(ctx, channel_name: str):
+    guild = ctx.guild
+    existing_channel = discord.utils.get(guild.channels, name=channel_name)
+    if not existing_channel:
+        print(f'Creating a new channel: {channel_name}')
+        await guild.create_text_channel(channel_name)
+
+@create_channel.error
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CheckFailure):
+        return await ctx.send('Sorry, you found a developer-only command!')
+
+bot.run(TOKEN)
+'''@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send('You are missing permissions for this command')'''
+
+
+'''@bot.command()
+async def help(ctx):
+    embed = discord.Embed(     
+        colour = discord.Colour.orange()
+    )
+    embed.set_author(name='help')
+    embed.add_field(name='ping',value='Show ping.', inline=True)
+
+    await ctx.author.send(embed=embed)'''
+'''key = '#'.join([message.author.name, message.author.discriminator])'''
+'''@commands.command(name="help", description="Returns all commands available")
+    async def help(self, ctx):
+        helptext = "```"
+        for command in self.bot.commands:
+            helptext+=f"{command}\n"
+        helptext+="```"
+        await ctx.send(helptext)''' 
+
+'''@bot.command(name='99', help='Responds with a random quote from Brooklyn 99')
 async def nine_nine(ctx):
     brooklyn_99_quotes = [
         'I\'m the human form of the :+1: emoji.',
@@ -65,18 +159,6 @@ async def nine_nine(ctx):
 
     response = random.choice(brooklyn_99_quotes)
     await ctx.send(response)
-
-@bot.command(name='roll_dice', help='Simulates rolling dice.')
-async def roll(ctx, number_of_dice: int, number_of_sides: int):
-    dice = [
-        str(random.choice(range(1, number_of_sides + 1)))
-        for _ in range(number_of_dice)
-    ]
-    await ctx.send(', '.join(dice))
-
-@bot.command(name='ping', help='Show ping.')
-async def ping(ctx):
-    await ctx.send(f'Pong! {round(bot.latency*1000)}ms')
 
 @bot.command()
 @commands.has_permissions(administrator=True)
@@ -95,58 +177,4 @@ async def displayembed(ctx, member: discord.Member):
 
     #await ctx.send(embed=embed)
     await member.create_dm()
-    await member.dm_channel.send(embed=embed) 
-  
-@bot.command(aliases=['create','cr'], help='Create a channel')
-@commands.has_role('admin')
-async def create_channel(ctx, channel_name: str):
-    guild = ctx.guild
-    existing_channel = discord.utils.get(guild.channels, name=channel_name)
-    if not existing_channel:
-        print(f'Creating a new channel: {channel_name}')
-        await guild.create_text_channel(channel_name)
-
-@create_channel.error
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.errors.CheckFailure):
-        await ctx.send(f'You do not have the correct role for this command')
-
-@bot.command(name='kick', help='Kick member.')
-@commands.has_permissions(administrator=True)
-async def kick(ctx, member : discord.Member, *, reason=None):
-    await member.kick(reason=reason)
-
-@bot.command(name='ban', help='Ban member.')
-@commands.has_permissions(administrator=True)
-async def ban(ctx, member : discord.Member, *, reason='Annoying'):
-    await member.ban(reason=reason)
-
-@bot.command(name='unban', help='Unban member.')
-@commands.has_permissions(administrator=True)
-async def unban(ctx, *, member):
-    banned_users = await ctx.guild.bans()
-    member_name, member_discriminator = member.split('#')
-
-    for ban_entry in banned_users:
-        user = ban_entry.user
-        if (user.name, user.discriminator) == (member_name, member_discriminator):
-            await ctx.guild.unban(user)
-            return
-            
-bot.run(TOKEN)
-'''@bot.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.MissingPermissions):
-        await ctx.send('You are missing permissions for this command')'''
-
-
-'''@bot.command()
-async def help(ctx):
-    embed = discord.Embed(     
-        colour = discord.Colour.orange()
-    )
-    embed.set_author(name='help')
-    embed.add_field(name='ping',value='Show ping.', inline=True)
-
-    await ctx.author.send(embed=embed)'''
-'''key = '#'.join([message.author.name, message.author.discriminator])'''
+    await member.dm_channel.send(embed=embed) '''
